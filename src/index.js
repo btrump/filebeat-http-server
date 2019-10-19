ace80b7f0523b45f1ae380c5ef6e927f0161bb1f
Blair Trump <btrump@vexcelco.com>
2019-10-19 15:54:11 -0400

    add basic auth

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const config = require('./config');
 const apiRouter = require('./routers/api');
+const basicAuth = require('express-basic-auth');
 const app = express();
 
+app.use(basicAuth({
+  users: { 'user': 'password' }
+}));
 app.use('/api', apiRouter);
 
 const server = app.listen(config.port, () => {
