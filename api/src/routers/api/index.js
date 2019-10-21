560923450af927de482397d78d193acd31c4fcec
Blair Trump <btrump@vexcelco.com>
2019-10-21 16:19:37 -0400

    Added Dockerfile for nginx server

--- api/src/routers/api/index.js
+++ api/src/routers/api/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.post('/', bodyParser.json({}), (req, res, next) => {
   console.log("[STATUS 200]: " + req.body.message);
-  res.status(200).send(req.body.message);
+  res.status(200).end();
 });
 
 module.exports = router;
